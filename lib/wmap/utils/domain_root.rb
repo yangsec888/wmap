2e4f1780936c371ad9df2d1f5c70e90a8fefbdbb
Li, Sam <sli@penguinrandomhouse.com>
2018-11-05 12:44:07 -0500

    Add support for complete list of IANA top level domains

--- lib/wmap/utils/domain_root.rb
+++ lib/wmap/utils/domain_root.rb
@@ -8,25 +8,29 @@
 
 
 module Wmap
- module Utils	
+ module Utils
   # Module to validate and retrieve the top or second level domain name from a host-name (FQDN).
-  module DomainRoot	
+  module DomainRoot
 	extend self
 	# Internet Domain Architecture Definitions
 	File_ccsld=File.dirname(__FILE__)+'/../../../dicts/ccsld.txt'
 	File_cctld=File.dirname(__FILE__)+'/../../../dicts/cctld.txt'
 	File_gtld=File.dirname(__FILE__)+'/../../../dicts/gtld.txt'
-	
+  File_tld=File.dirname(__FILE__)+'/../../../dicts/tlds.txt'
+
 	# Main function to retrieve the registered domain ('domain root' from the 'registrant' perspective) from a hostname, for example, "www.telegraph.co.uk" -> "telegraph.co.uk"
 	def get_domain_root (host)
 		puts "Retrieve the root domain for host: #{host}" if @verbose
 		begin
+      # Comnplete Top Level Domain List - loading once
+      @tlds=file_2_hash(File_tld) if @tlds.nil?
 			# Generic Top Level Domain List - loading once
 			@gtld=file_2_hash(File_gtld) if @gtld.nil?
 			# Country code top-level domain list - loading once
 			@cctld=file_2_hash(File_cctld) if @cctld.nil?
 			# Country code second level domain - loading once
 			@ccsld=load_ccsld_from_file(File_ccsld) if @ccsld.nil?
+
 			if host.strip.nil?
 				puts "Error: empty record found. Please check your input and remove any empty line." if @verbose
 				return nil
@@ -35,15 +39,15 @@ module Wmap
 			end
 			found_tld=false
 			found_cctld=false
-			# search the general top level domain list first
+			# search the  top level domain list first
 			root_domain=""
 			dn=host.split(".")
-			if @gtld.key?(dn.last) 
-				found=false
-				if @cctld.key?(dn[dn.length-2]) 
-					found=true
+			if @tlds.key?(dn.last)
+				cc_found=false
+				if @cctld.key?(dn[dn.length-2])
+					cc_found=true
 				end
-				if found
+				if cc_found
 					root_domain=dn[dn.length-3] + "." + dn[dn.length-2] + "." + dn.last
 				else
 					root_domain=dn[dn.length-2] + "." + dn.last
@@ -51,7 +55,7 @@ module Wmap
 				found_tld=true
 			end
 			# search the country code top level domain list secondly
-			if @cctld.key?(dn.last) 
+			if @cctld.key?(dn.last)
 				found=false
 				# reverse search of general top level domain
 				if @gtld.key?(dn[dn.length-2])
@@ -65,8 +69,8 @@ module Wmap
 							break
 						end
 					end
-					# 1/8/2015: additional logic to handle invalid ccsld string: reserved gtld string 
-					#unless found 
+					# 1/8/2015: additional logic to handle invalid ccsld string: reserved gtld string
+					#unless found
 					#	if @gtld.key?(dn[dn.length-2])
 					#		puts "Invalid ccsld: #{dn[dn.length-2]} for host: #{host}"
 					#		return nil
@@ -77,17 +81,17 @@ module Wmap
 					root_domain=dn[dn.length-3] + "." + dn[dn.length-2] + "." + dn.last
 				else
 					root_domain=dn[dn.length-2] + "." + dn.last
-				end	
+				end
 				found_cctld=true
 			end
-			unless (found_tld or found_cctld)  
+			unless (found_tld or found_cctld)
 				puts "#{host} - the top level domain is unknown. Please check out your record #{root_domain} " if @verbose
 				return nil
 			else
 				puts "Domain root found: #{root_domain}" if @verbose
 				return root_domain
 			end
-		rescue => ee			
+		rescue => ee
 			puts "Exception on method #{__method__}: #{ee}" if @verbose
 			return nil
 		end
@@ -96,7 +100,7 @@ module Wmap
 	alias_method :root_domain, :get_domain_root
 	alias_method :domain_root, :get_domain_root
 	alias_method :host_2_domain, :get_domain_root
-	
+
 	# 'setter' to parse and load the known country code second level domain table from the file
 	# data structure example: {"uk" =>["co","plc"],"za"=>["mil","nom","org"]}
 	def load_ccsld_from_file (file_ccsld)
@@ -107,10 +111,10 @@ module Wmap
 			f.each do |line|
 				next unless line =~ /^\s+\.\w/
 				line=line.chomp.strip.downcase
-				entry=line.split(' ')[0].split('.')				
+				entry=line.split(' ')[0].split('.')
 				if entry.length > 2
 					key=entry.last
-					ccsld[key] = Array.new if not ccsld.key?(key)				
+					ccsld[key] = Array.new if not ccsld.key?(key)
 					val=entry[entry.length-2]
 					#puts "Loading country code second level domain table with - Country code: #{key}, Second level domain: #{val}" if @verbose
 					ccsld[key].push(val) unless key.nil?
@@ -119,7 +123,7 @@ module Wmap
 			f.close
 			# Sort the blocks once in descendant order once for better performance
 			return ccsld
-		rescue => ee			
+		rescue => ee
 			puts "Exception on method #{__method__}: #{ee}" if @verbose
 		end
 	end
@@ -158,15 +162,15 @@ module Wmap
 		rescue Exception => ee
 			puts "Exception on method #{__method__} for #{host}: #{ee}" if @verbose
 			return nil
-		end		
+		end
 	end
 	alias_method :get_subdomain, :get_sub_domain
-	
+
 	# Function to print instance variable - General top level domain list
 	def print_gtld
 		puts @gtld
 	end
-	
+
 	# Function to print instance variable - Country code top-level domain list
 	def print_cctld
 		puts @cctld
@@ -176,9 +180,9 @@ module Wmap
 	def print_ccsld
 		puts @ccsld
 	end
-	
+
 	private :load_ccsld_from_file
-	 
+
   end
  end
 end
